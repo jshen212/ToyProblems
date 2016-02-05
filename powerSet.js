71c9cf58a3b343cf494a743930eed8fdb1af79ad
simplejeff <jeffshen86@gmail.com>
2016-02-04 18:01:22 -0800

    added instructions to powerSet.js

--- powerSet.js
+++ powerSet.js
@@ -1,3 +1,13 @@
+/*
+Return an array that contains the power set of a given string. The power set is a set of all the possible subsets, including the empty set.
+
+Make sure your code does the following:
+
+All characters in a subset should be sorted alphabetically, and the array of subsets should be sorted alphabetically.
+Sets of the same characters are considered duplicates regardless of order and count only once, e.g. ‘ab’ and ‘ba’ are the same.
+Duplicate characters in strings should be ignored; for example, ‘obama’ should be evaluated as if it only contained one ‘a’. See the result below.
+*/
+
 function powerSet(string) {
   var results = [''];
   noDupes = [];
