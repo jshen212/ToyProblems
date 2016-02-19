3d4e55fef807a3cd98eb2a6ba65efbb2ec33614f
simplejeff <jeffshen86@gmail.com>
2016-02-19 08:52:50 -0800

    [refactor] commonChars.js

--- commonChars.js
+++ commonChars.js
@@ -11,17 +11,15 @@ Returns: 'aeiou'
 
 
 commonCharacters = function(string1, string2){
-var results=[];
+  var results=[];
 
   var splitJoinStr2 = string2.split(' ').join('');
 
   for(var i = 0; i < string1.length; i++){
-     if(splitJoinStr2.indexOf(string1[i])!==-1){
-       if(results.indexOf(string1[i])===-1){
-         results.push(string1[i]);
-       }
+    if(splitJoinStr2.indexOf(string1[i])!==-1 && results.indexOf(string1[i])===-1){
+      results.push(string1[i]);
     }
   }
 
-   return results.join('');
+  return results.join('');
 };
