5bea8b67f62c97df17176ca0ed58b168683ad90a
simplejeff <jeffshen86@gmail.com>
2016-05-01 11:59:43 -0700

    added comments

--- spiralTraversal.js
+++ spiralTraversal.js
@@ -38,11 +38,55 @@ function spiralTraversal (matrix) {
           temp.push(matrix[i].pop());
         }
       }
-          rotatedMatrix.push(temp);
-          temp = [];
+      rotatedMatrix.push(temp);
+      temp = [];
     }
     return rotatedMatrix;
   }
 
   return result;
+}
+
+// solution with comments
+function spiralTraversal(matrix){
+  var results = [];
+
+  //   keep traversing as long as there are still values
+  while(matrix.length > 0){
+    // iterate over the 0 index array and push all values in order into results array
+    for(var i = 0; i < matrix[0].length; i++){
+      results.push(matrix[0][i]);
+    }
+    // remove 0 index array once all values are pushed into results
+    matrix.shift();
+
+    // rotate the rest of the matrix
+    matrix = rotate(matrix);
+  }
+
+
+  function rotate(matrix){
+    var temp = [];
+    var rotated = [];
+
+    while(matrix.length){
+      for(var i = 0; i < matrix.length; i++){
+        // if any inside array becomes empty, remove it
+        if(matrix[i].length === 0){
+          matrix.splice(i,1);
+        }
+        // otherwise, pop off the last value and store in temp
+        else {
+          temp.push(matrix[i].pop());
+        }
+      }
+
+      // push temp array into rotated array and reset temp to empty so it can continue pushing the rest of the matrix in without duplicating
+      rotated.push(temp);
+      temp = [];
+    }
+
+    return rotated;
+  }
+  return results;
 }
